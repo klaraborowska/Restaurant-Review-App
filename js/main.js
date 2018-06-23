5286b264145003d4cc9523fa21bb47307871a6b7
Klara <klara.borowska@gmail.com>
2018-06-23 02:01:29 +0200

    Improve accessibility

--- js/main.js
+++ js/main.js
@@ -167,6 +167,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute("aria-label", `View '${restaurant.name}' details`);
+  more.setAttribute("role", "link");
   li.append(more)
 
   return li
